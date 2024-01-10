e6e83453064fcc2f6d6ab4b78bfffc51c9a09d09
Roshanka <roshanka32@gmail.com>
2024-01-10 10:33:12 -0800

    Enroll Lesoons Plan UI

---
+++ Enroll_LessonPlans.Designer.vb
@@ -0,0 +1,435 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Enroll_LessonPlans
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Enroll_LessonPlans))
+        Panel4 = New Panel()
+        Panel6 = New Panel()
+        Button8 = New Button()
+        Button7 = New Button()
+        Button6 = New Button()
+        Button5 = New Button()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Panel3 = New Panel()
+        Button9 = New Button()
+        Label15 = New Label()
+        Label11 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label7 = New Label()
+        Label4 = New Label()
+        Button1 = New Button()
+        Panel5 = New Panel()
+        SignUpbtn = New Label()
+        Panel1 = New Panel()
+        Label2 = New Label()
+        Label1 = New Label()
+        Label3 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Panel2 = New Panel()
+        Panel4.SuspendLayout()
+        Panel6.SuspendLayout()
+        Panel3.SuspendLayout()
+        Panel5.SuspendLayout()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackgroundImage = CType(resources.GetObject("Panel4.BackgroundImage"), Image)
+        Panel4.Controls.Add(Panel2)
+        Panel4.Controls.Add(Panel6)
+        Panel4.Controls.Add(Panel3)
+        Panel4.Dock = DockStyle.Fill
+        Panel4.Location = New Point(0, 80)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(1080, 640)
+        Panel4.TabIndex = 9
+        ' 
+        ' Panel6
+        ' 
+        Panel6.Controls.Add(Button8)
+        Panel6.Controls.Add(Button7)
+        Panel6.Controls.Add(Button6)
+        Panel6.Controls.Add(Button5)
+        Panel6.Controls.Add(Button4)
+        Panel6.Controls.Add(Button3)
+        Panel6.Controls.Add(Button2)
+        Panel6.Location = New Point(0, 0)
+        Panel6.Name = "Panel6"
+        Panel6.Size = New Size(250, 581)
+        Panel6.TabIndex = 1
+        ' 
+        ' Button8
+        ' 
+        Button8.FlatAppearance.BorderSize = 0
+        Button8.FlatStyle = FlatStyle.Flat
+        Button8.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button8.Location = New Point(47, 504)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(101, 57)
+        Button8.TabIndex = 6
+        Button8.Text = "Sign Out"
+        Button8.TextAlign = ContentAlignment.MiddleLeft
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.FlatAppearance.BorderSize = 0
+        Button7.FlatStyle = FlatStyle.Flat
+        Button7.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button7.Location = New Point(47, 357)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(101, 57)
+        Button7.TabIndex = 5
+        Button7.Text = "Records"
+        Button7.TextAlign = ContentAlignment.MiddleLeft
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.FlatAppearance.BorderSize = 0
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button6.Location = New Point(47, 295)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(101, 57)
+        Button6.TabIndex = 4
+        Button6.Text = "Lessons"
+        Button6.TextAlign = ContentAlignment.MiddleLeft
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.FlatAppearance.BorderSize = 0
+        Button5.FlatStyle = FlatStyle.Flat
+        Button5.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(47, 233)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(126, 57)
+        Button5.TabIndex = 3
+        Button5.Text = "Instructors"
+        Button5.TextAlign = ContentAlignment.MiddleLeft
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.FlatAppearance.BorderSize = 0
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(47, 171)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(117, 57)
+        Button4.TabIndex = 2
+        Button4.Text = "Payments"
+        Button4.TextAlign = ContentAlignment.MiddleLeft
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.FlatAppearance.BorderSize = 0
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        Button3.Location = New Point(47, 108)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(138, 57)
+        Button3.TabIndex = 1
+        Button3.Text = "Lesson Plans"
+        Button3.TextAlign = ContentAlignment.MiddleLeft
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.FlatAppearance.BorderSize = 0
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.ForeColor = Color.Black
+        Button2.Location = New Point(47, 46)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 57)
+        Button2.TabIndex = 0
+        Button2.Text = "Profile"
+        Button2.TextAlign = ContentAlignment.MiddleLeft
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.White
+        Panel3.Controls.Add(Label5)
+        Panel3.Controls.Add(Button9)
+        Panel3.Controls.Add(Label15)
+        Panel3.Controls.Add(Label11)
+        Panel3.Controls.Add(Label10)
+        Panel3.Controls.Add(Label9)
+        Panel3.Controls.Add(Label7)
+        Panel3.Controls.Add(Label4)
+        Panel3.Controls.Add(Button1)
+        Panel3.Controls.Add(Panel5)
+        Panel3.Location = New Point(439, 20)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(425, 541)
+        Panel3.TabIndex = 0
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.Red
+        Button9.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button9.ForeColor = Color.Black
+        Button9.Location = New Point(108, 465)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(105, 38)
+        Button9.TabIndex = 21
+        Button9.Text = "Cancel"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.FlatStyle = FlatStyle.Flat
+        Label15.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label15.ForeColor = Color.Black
+        Label15.Location = New Point(24, 363)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(121, 23)
+        Label15.TabIndex = 20
+        Label15.Text = "Data Scientist"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label11.ForeColor = Color.Red
+        Label11.Location = New Point(24, 335)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(36, 18)
+        Label11.TabIndex = 15
+        Label11.Text = "Job"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.FlatStyle = FlatStyle.Flat
+        Label10.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Label10.ForeColor = Color.Black
+        Label10.Location = New Point(24, 234)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(382, 23)
+        Label10.TabIndex = 14
+        Label10.Text = "3 Practical Lessons and 3 Theory Lessons(12 H)"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.FlatStyle = FlatStyle.Flat
+        Label9.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        Label9.ForeColor = Color.Black
+        Label9.Location = New Point(24, 143)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(89, 25)
+        Label9.TabIndex = 13
+        Label9.Text = "Standard"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.ForeColor = Color.Red
+        Label7.Location = New Point(24, 205)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(69, 18)
+        Label7.TabIndex = 9
+        Label7.Text = "Address"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.Red
+        Label4.Location = New Point(24, 116)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(104, 18)
+        Label4.TabIndex = 7
+        Label4.Text = "Course Type"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(0))
+        Button1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(219, 465)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(105, 38)
+        Button1.TabIndex = 5
+        Button1.Text = "Enroll"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Panel5
+        ' 
+        Panel5.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(0))
+        Panel5.Controls.Add(SignUpbtn)
+        Panel5.Dock = DockStyle.Top
+        Panel5.Location = New Point(0, 0)
+        Panel5.Name = "Panel5"
+        Panel5.Size = New Size(425, 53)
+        Panel5.TabIndex = 0
+        ' 
+        ' SignUpbtn
+        ' 
+        SignUpbtn.AutoSize = True
+        SignUpbtn.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        SignUpbtn.Location = New Point(168, 11)
+        SignUpbtn.Name = "SignUpbtn"
+        SignUpbtn.Size = New Size(77, 31)
+        SignUpbtn.TabIndex = 0
+        SignUpbtn.Text = "Enroll"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Dock = DockStyle.Top
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1080, 80)
+        Panel1.TabIndex = 8
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(56, 19)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(261, 41)
+        Label2.TabIndex = 1
+        Label2.Text = "IT Driving School"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(239, 198)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(53, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(852, 20)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(172, 17)
+        Label3.TabIndex = 7
+        Label3.Text = "@2023 All Rights Reserved"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.FlatStyle = FlatStyle.Flat
+        Label5.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold)
+        Label5.ForeColor = Color.Black
+        Label5.Location = New Point(24, 264)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(316, 23)
+        Label5.TabIndex = 22
+        Label5.Text = "You can arrange meetings as you want"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(852, 20)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(172, 17)
+        Label6.TabIndex = 0
+        Label6.Text = "@2023 All Rights Reserved"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Panel2.Controls.Add(Label6)
+        Panel2.Dock = DockStyle.Bottom
+        Panel2.Location = New Point(0, 581)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(1080, 59)
+        Panel2.TabIndex = 2
+        ' 
+        ' Enroll_LessonPlans
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1080, 720)
+        Controls.Add(Panel4)
+        Controls.Add(Panel1)
+        Controls.Add(Label3)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Enroll_LessonPlans"
+        Text = "Enroll_LessonPlans"
+        Panel4.ResumeLayout(False)
+        Panel6.ResumeLayout(False)
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        Panel5.ResumeLayout(False)
+        Panel5.PerformLayout()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents SignUpbtn As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Enroll_LessonPlans.vb
@@ -0,0 +1,3 @@
+﻿Public Class Enroll_LessonPlans
+
+End Class
